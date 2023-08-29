95211c7a7b33eb3255387fd75b3fb29f47e200e9
javtronic <juniorjavi7@gmail.com>
2023-08-29 12:50:05 -0500

    Fin seccion 17

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
+
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -15,15 +22,47 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   })
 
   constructor(private fb: FormBuilder){}
 
+  ngOnInit(): void {
+  //  this.myForm.reset(rtx5090);
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string ): string | null {
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracters.`;
+      }
+    }
+    return null;
+  }
+
   onSave():void {
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();  
+      return;
+    }
+
     console.log(this.myForm.value);
     
+    //reset strings vacios por defecto
+    this.myForm.reset({price: 0, inStorage: 0});
   }
 }
